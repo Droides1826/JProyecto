5962a582e5c1a824f25d79206260cbce783cd6e8
Droides1826 <jesus_dgonzalez@soy.sena.co>
2025-02-11 11:37:37 -0500

    FINISH: se arreglaron todas las apis como se requeria se ha finalizado todo

--- routes/categorias.py
+++ routes/categorias.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request
 from utils.respuestas import respuesta_json_success, respuesta_json_fail
-from services.categorias_service import obtener_categorias, ingresar_categoria
+from services.categorias_service import obtener_categorias, ingresar_categoria, actualizar_categoria, cambiar_estado_categoria	
 from utils.Validaciones import (es_solo_letras,tiene_caracteres_especiales,limite_caracteres
 )
 
@@ -58,7 +58,60 @@ def create_categoria():
     except Exception as e:
         return respuesta_json_fail(str(e), 400)
 
+@categorias.route("/actualizar_categorias", methods=["POST, GET"])
+def actualizar_categorias():
+    try:
+        datos = request.get_json()
+        if not datos:
+            return respuesta_json_fail("No se enviaron datos para actualizar.", 400)
 
-@categorias.route("/index")
-def index():
-    return render_template("index.html")
+        valores_categorias = {
+            "id_categoria": datos.get("id_categoria"),
+            "nombre": datos.get("nombre", "").strip(),
+            "descripcion": datos.get("descripcion", "").strip(),
+        }
+        if valores_categorias["nombre"]:
+            if not es_solo_letras(valores_categorias["nombre"]):
+                return respuesta_json_fail("El nombre solo puede contener letras.", 400)
+            if tiene_caracteres_especiales(valores_categorias["nombre"]):
+                return respuesta_json_fail("El nombre no puede contener caracteres especiales.", 400)
+            if not limite_caracteres(valores_categorias["nombre"], 30):
+                return respuesta_json_fail("El nombre no puede tener más de 30 caracteres.", 400)
+            
+        if valores_categorias["descripcion"]:
+            if not limite_caracteres(valores_categorias["descripcion"], 255):
+                return respuesta_json_fail("La descripción no puede tener más de 255 caracteres.", 400)
+
+        filas_afectadas = actualizar_categoria(valores_categorias)
+
+        if filas_afectadas == 0:
+            return respuesta_json_fail("No se encontró la categoría o no hubo cambios.", 404)
+
+        return respuesta_json_success({"mensaje": "Categoría actualizada exitosamente"}, 200)
+
+    except Exception as e:
+        return respuesta_json_fail(str(e), 500)
+    
+@categorias.route("/cambiar_estado_categoria", methods=["POST"])
+def cambiar_estado_categorias():
+    try:
+        datos = request.get_json()
+        if not datos:
+            return respuesta_json_fail("No se enviaron datos para actualizar.", 400)
+
+        valores_categorias = {
+            "id_categoria": datos.get("id_categoria"),
+            "estado": datos.get("estado"),
+        }
+
+        filas_afectadas = cambiar_estado_categoria(valores_categorias)
+
+        if filas_afectadas == 0:
+            return respuesta_json_fail("No se encontró la categoría o no hubo cambios.", 404)
+
+        return respuesta_json_success({"mensaje": "Categoría actualizada exitosamente"}, 200)
+
+    except Exception as e:
+        return respuesta_json_fail(str(e), 500)
+
+
